7caa706f45dc762b57439fd936b334e23244636c
candice <dandelionqing@163.com>
2019-06-12 16:03:59 +0800

    add shell

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-mobx
+- bash install.sh
+- npm start
+- 在浏览器中打开src/index.html
---
+++ install.sh
@@ -0,0 +1,4 @@
+npm i webpack webpack-cli babel-core babel-preset-env babel-loader babel-plugin-transform-class-properties -D 
+npm i babel-plugin-transform-decorators-legacy -D
+npm i mobx react react-dom prop-types mobx-react -S
+npm i babel-preset-react -D
